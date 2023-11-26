447d60795ac2d1342082dd2bf7ca334664fc7ce4
matboud <m.matboud@gmail.com>
2023-11-26 16:54:35 +0100

    init backend

---
+++ lobbyx-backend/src/main/java/com/lobbyx/lobbyx/backend/LobbyxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lobbyx.lobbyx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LobbyxBackendApplication.class, args);
+    }
+
+}
